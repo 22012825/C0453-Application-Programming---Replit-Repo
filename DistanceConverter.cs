204b04fcbea96214d6b9eee33d5a33e2e2e46200
Amie Shanaya Wijeratne <72076192+22012825@users.noreply.github.com>
2022-02-24 11:14:35 +0000

    Initial commit

---
+++ DistanceConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleAppProject.App01
+{
+    /// <summary>
+    /// Please describe the main features of this App
+    /// </summary><This App allows the user to convert distances measured in one unit of distance into another unit of distance, for example it will convert a distance measured in miles into the same distance measured in feet </>
+
+    /// Amie version 0.1
+    public static class Program
+    public class DistanceConverter 
+    {
+        private double miles;
+        private double feet;
+
+        /// <summary>
+        /// Prompt the user to enter the distance in miles
+        /// Input the miles as a double number
+        /// <summary>
+        public void Run()
+
+
+        {
+            InputMiles();
+            {
+              ConsoleAppProject.  
+            }
+            CalculateFeet();
+            OutputFeet();
+        }
+
+        
+        private void InputMiles()
+        {
+          Console.Write("Please enter the number of miles >");
+          string value = Console.ReadLine();
+          miles = Convert.ToDouble(value);
+        }
+    private void CalculateFeet()
+    {
+
+    }
+
+    private void  OutputFeet()
+    
+
+
+    
+
+    
+
+
+    
+}
---
+++ DistanceUnits.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleAppProject.App01
+{
+    /// <summary>
+    /// List of units used to measure distance
+    /// </summary>
+    public enum DistanceUnits
+    {
+        [Display(Name = "Distance Converter")]
+
+
+
+
+
+
+
+
+
+
+
+    public void Run()
+    
+        private void EnterUnit() (15)
+    {
+        Console.Write("")
+
+
+
+    }
+
+    ]
+        
+        Feet,
+        Metres,
+        Kilometres,
+        Miles,
+    
+
+    private void Input feet
+    }
+
+
+
+
+
+
+
+
+    
+}
